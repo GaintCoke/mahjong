85a266ab2dd7c4f7e8da4b4c9caf4a2e5c961783
GaintCoke <18640519725@163.com>
2019-02-13 19:15:25 -0500

    test

---
+++ mahjong/mahjong.java
@@ -0,0 +1,13 @@
+package com.example.limingxu.mahjong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mahjong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mahjong);
+    }
+}
